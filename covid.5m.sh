e3f7a8aab783520435d8d2f96ae0de122979ca9b
silentbugs <d.misdanitis@gmail.com>
2020-12-01 23:59:51 +0200

    change covid script source

--- covid.5m.sh
+++ covid.5m.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 TERM=/usr/bin/screen
 
-# pass date as argument, in YYYYmmdd format, e.g. 20200715
+# pass date as argument, in dd-mm-YYY format, e.g. 15-07-2020
 function getCovidCases() {
-	curl -s https://eody.gov.gr/$1_briefing_covid19/?print=print | grep 'Σήμερα ανακοινώνουμε' | tr " " "\n" | grep strong | tr -d "</strong>,"
+	# 1. Ημερομ - Ημερομηνία
+	# 2. ΚρΗμ - Κρούσματα Ημέρας
+	# 3. ΚρΣυν - Σύνολο Κρουσμάτων
+	# 4. ΤεΗ - Κλινικά Τεστ Ημέρας
+	# 5. ΤεΣ - Κλινικά Τεστ Σύνολο
+	# 6. ΔεΘετ - Δείκτης θετικότητας (%) επί των κλινικών τεστ\
+	# 7. ΔΜ - Διασωληνωμένοι ΜΕΘ
+	# 8. ΘαΗ - Θάνατοι ημέρας
+	# 9. ΘαΣ - Θάνατοι σύνολο
+	curl -s http://www.odigostoupoliti.eu/koronoios-krousmata-simera-$1-stin-ellada/ \
+		| xmllint --html --xpath '//div[contains(@class, "entry-content")]/table//tr[2]' 2>/dev/null - \
+		| sed 's/<strong>//g' \
+		| sed 's/<\/strong>//g' \
+		| sed 's/<tr>//g' \
+		| sed 's/<\/tr>//g' \
+		| sed 's/<td>//g' \
+		| sed 's/<\/td>//g' \
+		| tr '\r\n' ' ' \
+		| awk 'BEGIN{FS=" "} {print $2,$4,$6,$7,$8}'
 }
 
 RED="$(tput setaf 1)"
 GREEN="$(tput setaf 2)"
 
-today=$(date +%Y%m%d)
-yesterday=$(date -v-1d +%Y%m%d)
+today=$(date +%d-%m-%Y)
+yesterday=$(date -v-1d +%d-%m-%Y)
 
 today_cases=$(getCovidCases $today | tr "\n" " ")
 yesterday_cases=$(getCovidCases $yesterday | tr "\n" " ")
