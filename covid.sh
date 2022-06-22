46079a35c8ab621bde2b6ad71cfd4438318fc80e
silentbugs <d.misdanitis@gmail.com>
2022-06-23 01:51:51 +0300

    fix line descriptions, create vertical xbar card output with submenus

--- covid.sh
+++ covid.sh
@@ -5,8 +5,8 @@ function getCovidCases() {
 	# 1. Ημερομ - Ημερομηνία
 	# 2. ΚρΗμ - Κρούσματα Ημέρας
 	# 3. ΚρΣυν - Σύνολο Κρουσμάτων
-	# 4. ΤεΗ - Κλινικά Τεστ Ημέρας
-	# 5. ΤεΣ - Κλινικά Τεστ Σύνολο
+	# 4. ΤεPCR - PCR τεστ ημέρας
+	# 5. ΤεΣPR - PCR και RAPID τεστ σύνολο
 	# 6. ΔεΘετ - Δείκτης θετικότητας (%) επί των κλινικών τεστ\
 	# 7. ΔΜ - Διασωληνωμένοι ΜΕΘ
 	# 8. ΘαΗ - Θάνατοι ημέρας
@@ -21,13 +21,37 @@ function getCovidCases() {
 		| sed 's/<td>//g' \
 		| sed 's/<\/td>//g' \
 		| tr '\r\n' ' ' \
-		| awk 'BEGIN{FS=" "} {print $2,$4,$6,$7,$8}'
+		| awk 'BEGIN{FS=" "} {print $1,$2,$3,$4,$5,$6,$7,$8,$9}'
 }
 
 function printResults() {
+	# replace nbsp with regular whitespace
+	cases=$(echo $1 | sed 's/\xc2\xa0/ /g')
+	# replace multiple whitespaces with single whitespace
+	cases=$(echo $cases | sed 's/ +/ /g')
+
+	# prepare the output for xbar
+	cases=$(echo $cases | awk 'BEGIN{FS=" "} {
+		print "---"
+		print "Daily | font=Hack"
+		print "---"
+		print "Date:   "$1" | font=Hack"
+		print "Cases:  "$2" | font=Hack"
+		print "Tests:  "$5" | font=Hack"
+		print "--PCR:    "$4" | font=Hack"
+		print "--Rapid:  "$5-$4" | font=Hack"
+		print "Rate:   "$6" | font=Hack"
+		print "ICU:    "$7" | font=Hack"
+		print "Deaths: "$8" | font=Hack"
+		print "---"
+		print "Totals | font=Hack"
+		print "---"
+		print "Cases:  "$3" | font=Hack"
+		print "Deaths: "$9" | font=Hack"
+	}')
 	echo "covid | color=$2"
 	echo "---"
-	echo "$1 | size=12"
+	echo "$cases"
 }
 
 today=$(date +%d-%m-%Y)
